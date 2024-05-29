78282e8b6983933a5305472bea33667f67c47b9d
wavy-cat <52543076+wavy-cat@users.noreply.github.com>
2024-05-29 11:53:59 +0500

    Полная (или нет) реализация Storage

--- backend/internal/storage/storage.go
+++ backend/internal/storage/storage.go
@@ -4,14 +4,32 @@ import "sync"
 
 // Storage примитивная база данных типа "ключ: значение"
 type Storage struct {
-	database map[string]interface{}
+	database map[string]any
 	mutex    sync.RWMutex
 }
 
 // NewStorage Конструктор для структуры Storage
 func NewStorage() *Storage {
 	return &Storage{
-		database: make(map[string]interface{}),
+		database: make(map[string]any),
 		mutex:    sync.RWMutex{},
 	}
+}
+
+func (s *Storage) Set(key string, value any) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.database[key] = value
+}
+
+func (s *Storage) Get(key string) any {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return s.database[key]
+}
+
+func (s *Storage) Delete(key string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete(s.database, key)
 }
